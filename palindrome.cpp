2402df2c649d3e04f54a840f99857895ae815561
Fatma Mohamed <fatymahmohamed@gmail.com>
2018-02-09 08:33:18 +0300

    added palindrome file

---
+++ palindrome.cpp
@@ -0,0 +1,55 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool isPalindrome = true ;
+    
+    const char mstring
+    const char mstart
+    const char mend
+
+    
+       
+       const char = mstart = mstring;
+       const char = mend = mstring;
+       
+       if ( NULL == mstring)
+       
+       return false;
+       
+       (!=mstring)
+       
+       return false;
+    
+       
+       
+       
+       while(mstart < mend)
+       { 
+           if (mstart != mend)
+           return false ;
+           
+           --mend;
+           ++mstart;
+           
+       }
+       return true ;
+       
+       
+       
+
+    return 0;
+}
+
+
+
