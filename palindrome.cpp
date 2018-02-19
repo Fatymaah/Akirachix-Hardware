0417153f8b7a4fcd5bbf12e37291eb073d65f6ea
Fatma Mohamed <fatymahmohamed@gmail.com>
2018-02-19 12:55:15 +0300

    The right way

--- palindrome.cpp
+++ palindrome.cpp
@@ -7,49 +7,27 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main()
 {
-    bool isPalindrome = true ;
+    cout<<"Enter word to check \n";
+    char word[20];
     
-    const char mstring
-    const char mstart
-    const char mend
-
-    
-       
-       const char = mstart = mstring;
-       const char = mend = mstring;
-       
-       if ( NULL == mstring)
-       
-       return false;
-       
-       (!=mstring)
-       
-       return false;
-    
-       
-       
-       
-       while(mstart < mend)
-       { 
-           if (mstart != mend)
-           return false ;
-           
-           --mend;
-           ++mstart;
-           
-       }
-       return true ;
-       
-       
-       
+    cin>> word;
+    cout << word; cout << "\n";
+    int array_size, array_size1;    
+    array_size = strlen(word);
+    cout << "Size: "; cout << array_size; cout << "\n";
+    for(int i = 0; i < (array_size); i++) {
+        if(word[i] != word[array_size - (i+1)]) {
+            cout << word; cout << " is NOT a palindrome! \n";
+            return 0;
+        }
+    }
+    cout << word; cout << " is a palindrome!";
 
     return 0;
 }
-
-
-
